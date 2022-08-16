dd92268f07359b4c4dc70f700cc1776449738971
Loup Blanc <nistrade7@gmail.com>
2022-08-16 19:50:41 +0200

    changement

--- Cooper.py
+++ Cooper.py
@@ -143,8 +143,7 @@ async def unmute(ctx, member : discord.Member, *, reason = "Aucune raison n'a é
 
 @bot.command()
 async def help(ctx):
-    embed = discord.Embed(title="Help", description="Voici toute les commandes proposer par le bot"
-    "|!ban| |!clear| |!help| |!interval| |!kick| |!mute| |!unban| |!unmute|")
+    embed = discord.Embed(title="Help", description="Voici toute les commandes proposer par le bot |!ban| |!clear| |!help| |!interval| |!kick| |!mute| |!unban| |!unmute|", inline = True)
     embed.set_thumbnail(url="https://tse1.mm.bing.net/th?id=OIP.R5NtROuWMQugf9qYKouA5gHaIU&pid=Api&P=0")
     await ctx.send(embed=embed)
 
