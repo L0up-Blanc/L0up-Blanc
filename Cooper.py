ea8ee385d632f705aba3dad371f2fb36df329c3e
Loup Blanc <nistrade7@gmail.com>
2022-08-16 22:17:55 +0200

    changement

--- Cooper.py
+++ Cooper.py
@@ -52,7 +52,7 @@ async def skip(ctx):
     client.stop()
 
 def play_song(client, queue, song):
-    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(song.stream_url, before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_deplay_max 5"))
+    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(song.stream_url, before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"))
     def next(_):
         if len(queue) > 0:
             new_song = queue[0]
@@ -60,7 +60,6 @@ def play_song(client, queue, song):
             play_song(client, queue, new_song)
         else:
             asyncio.run_coroutine_threadsafe(client.disconnect(), bot.loop)
-
     client.play(source, after=next)
 
 @bot.command()
