2a4004fb50a75e5b6135543d816cfc7f54ee3624
Loup Blanc <nistrade7@gmail.com>
2022-08-16 18:54:21 +0200

    changement

--- Cooper.py
+++ Cooper.py
@@ -126,14 +126,16 @@ async def getMutedRole(ctx):
             return role
     return await createMutedRole(ctx)
 @bot.command()
+@commands.has_permissions(mute_members=True)
 async def mute(ctx, member : discord.Member, *, reason = "Aucune raison n'a été renseigné"):
     mutedRole = await getMutedRole(ctx)
     await member.add_roles(mutedRole, reason = reason)
     await ctx.send(f"{member.mention} a été mute")
 
 @bot.command()
+@commands.has_permissions(mute_members=True)
 async def unmute(ctx, member : discord.Member, *, reason = "Aucune raison n'a été renseigné"):
     mutedRole = await getMutedRole(ctx)
     await member.remove_roles(mutedRole, reason = reason)
-    
+
 bot.run("MTAwODMzMDg1NTA3ODQ0NTA4Ng.G3oXWO.ka76kAjrUm7k2Q8BfiRivl3Mpzn4qTFOcl4HcE")
