9392ef9252b559d35314f2b7f9117c20498d6dfb
Loup Blanc <nistrade7@gmail.com>
2022-08-20 23:39:23 +0200

    changement

--- Cooper.py
+++ Cooper.py
@@ -122,9 +122,10 @@ async def on_message_delete(message):
 async def on_message_edit(before, after):
     logs_channel = discord.utils.get(before.guild.channels, name="logs")
     embed = discord.Embed(titre = "**Message editer**")
+    embed.set_author(name = before.author, icon_url = before.author.avatar_url)
     embed.add_field(name = "Message d'avant", value = before.content, inline = True)
     embed.add_field(name = "Message modifier", value = after.content, inline = True)
-    await logs_channel.send(embed = embed)
+    await logs_channel.send(embed = embed )
 
 @bot.event 
 async def on_typing(channel, user, when):
